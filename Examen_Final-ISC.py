b3ad270c9b3c8858795cc59e6be5c6b99129770c
unknown <appstutorialesy@gmail.comm>
2020-06-10 22:49:13 -0600

    Creacion del titulo del programa

--- Examen_Final-ISC.py
+++ Examen_Final-ISC.py
@@ -10,3 +10,6 @@ class Desk:
         alt = 225
         self.wind = window
         self.wind.geometry(str(anc)+'x'+str(alt))
+        #Titulo del programa
+        self.wind.columnconfigure(0, weight=1)
+        self.wind.title('Examen Final')
